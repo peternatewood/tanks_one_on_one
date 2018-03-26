17c301b084834a7a61a48d2a5352eea0b096e681
Peter Wood <pwood@sankyinc.com>
2018-03-26 17:29:10 -0400

    Draw tank shells

--- app/src/main/java/com/peternatewood/tanksone_on_one/GameView.java
+++ app/src/main/java/com/peternatewood/tanksone_on_one/GameView.java
@@ -36,7 +36,7 @@ public class GameView extends SurfaceView implements Runnable {
     Color.parseColor("#DDEE22"), // Yellow fill
     Color.parseColor("#778800")  // Yellow outline
   };
-  private final int TILE_SIZE = 32;
+  static final int TILE_SIZE = 32;
   private final float SCALED_TILE, TILES_Y;
   private final int LEVEL_SIZE = 500;
   private final int[][] LEVELS = {
@@ -86,8 +86,8 @@ public class GameView extends SurfaceView implements Runnable {
     tank1 = new Tank( 64.f,  64.f, 0.f, TILE_SIZE);
     tank2 = new Tank(576.f, 576.f, (float) Math.PI, TILE_SIZE);
 
-    joy1 = new Joystick(w, h, true);
-    joy2 = new Joystick(w, h, false);
+    joy1 = new Joystick(w, h, false);
+    joy2 = new Joystick(w, h, true);
 
     surfaceHolder = getHolder();
     paint = new Paint();
@@ -106,6 +106,12 @@ public class GameView extends SurfaceView implements Runnable {
   private void update() {
     tank1.update(LEVELS[0], tank2);
     tank2.update(LEVELS[0], tank1);
+
+    if (tank1._life() == 0 || tank2._life() == 0) {
+      // Reset
+      tank1.reset();
+      tank2.reset();
+    }
   }
 
   private void draw() {
@@ -134,8 +140,27 @@ public class GameView extends SurfaceView implements Runnable {
         }
       }
 
-      drawTank(tank1);
-      drawTank(tank2);
+      drawTank(tank1, 0);
+      drawTank(tank2, 2);
+
+      paint.setColor(Color.WHITE);
+      Shell[] shells1 = tank1._shells();
+      Shell[] shells2 = tank2._shells();
+      float sX, sY;
+      float size = 2 * scale;
+
+      for (int i = 0; i < 3; i++) {
+        if (shells1[i]._life() > 0) {
+          sX = scale * shells1[i]._x();
+          sY = TILES_Y + scale * shells1[i]._y();
+          canvas.drawRect(sX - size, sY - size, sX + size, sY + size, paint);
+        }
+        if (shells2[i]._life() > 0) {
+          sX = scale * shells2[i]._x();
+          sY = TILES_Y + scale * shells2[i]._y();
+          canvas.drawRect(sX - size, sY - size, sX + size, sY + size, paint);
+        }
+      }
 
       drawJoystick(joy1);
       drawJoystick(joy2);
@@ -145,40 +170,42 @@ public class GameView extends SurfaceView implements Runnable {
     }
   }
 
-  private void drawTank(Tank tank) {
-    // Draw tank fill
-    Path tankPath = new Path();
-    float[] points = tank.points();
+  private void drawTank(Tank tank, int colorIndex) {
+    if (tank.isAlive()) {
+      // Draw tank fill
+      Path tankPath = new Path();
+      float[] points = tank.points();
 
-    int pointsCount = tank.pointsCount();
-    tankPath.moveTo(scale * points[0], TILES_Y + scale * points[1]);
-    for (int i = 1; i < pointsCount; i++) {
-      tankPath.lineTo(scale * points[2 * i], TILES_Y + scale * points[2 * i + 1]);
-    }
-    paint.setColor(TANK_PALETTE[0]);
-    paint.setStyle(Style.FILL);
-    canvas.drawPath(tankPath, paint);
+      int pointsCount = tank.pointsCount();
+      tankPath.moveTo(scale * points[0], TILES_Y + scale * points[1]);
+      for (int i = 1; i < pointsCount; i++) {
+        tankPath.lineTo(scale * points[2 * i], TILES_Y + scale * points[2 * i + 1]);
+      }
+      paint.setColor(TANK_PALETTE[colorIndex]);
+      paint.setStyle(Style.FILL);
+      canvas.drawPath(tankPath, paint);
 
-    // Draw tank outline
-    float x1, y1, x2, y2;
-    float[] outline = tank.outline();
-    paint.setColor(TANK_PALETTE[1]);
-    for (int i = 0; i < pointsCount; i++) {
-      x1 = scale * outline[2 * i];
-      y1 = TILES_Y + scale * outline[2 * i + 1];
-      x2 = scale * outline[(2 * i + 2) % (2 * pointsCount)];
-      y2 = TILES_Y + scale * outline[(2 * i + 3) % (2 * pointsCount)];
-      canvas.drawLine(x1, y1, x2, y2, paint);
-    }
+      // Draw tank outline
+      float x1, y1, x2, y2;
+      float[] outline = tank.outline();
+      paint.setColor(TANK_PALETTE[colorIndex + 1]);
+      for (int i = 0; i < pointsCount; i++) {
+        x1 = scale * outline[2 * i];
+        y1 = TILES_Y + scale * outline[2 * i + 1];
+        x2 = scale * outline[(2 * i + 2) % (2 * pointsCount)];
+        y2 = TILES_Y + scale * outline[(2 * i + 3) % (2 * pointsCount)];
+        canvas.drawLine(x1, y1, x2, y2, paint);
+      }
 
-    // Draw tank turret
-    float[] turret = tank.turret();
-    for (int i = 0; i < pointsCount; i++) {
-      x1 = scale * turret[2 * i];
-      y1 = TILES_Y + scale * turret[2 * i + 1];
-      x2 = scale * turret[(2 * i + 2) % (2 * pointsCount)];
-      y2 = TILES_Y + scale * turret[(2 * i + 3) % (2 * pointsCount)];
-      canvas.drawLine(x1, y1, x2, y2, paint);
+      // Draw tank turret
+      float[] turret = tank.turret();
+      for (int i = 0; i < pointsCount; i++) {
+        x1 = scale * turret[2 * i];
+        y1 = TILES_Y + scale * turret[2 * i + 1];
+        x2 = scale * turret[(2 * i + 2) % (2 * pointsCount)];
+        y2 = TILES_Y + scale * turret[(2 * i + 3) % (2 * pointsCount)];
+        canvas.drawLine(x1, y1, x2, y2, paint);
+      }
     }
   }
 
@@ -226,17 +253,32 @@ public class GameView extends SurfaceView implements Runnable {
     int index = event.getActionIndex();
     float x = event.getX(index);
     float y = event.getY(index);
+    boolean buttonState[] = { joy1._button(), joy2._button() };
 
     switch (event.getAction() & MotionEvent.ACTION_MASK) {
       case MotionEvent.ACTION_DOWN:
         // First touch event
         joy1.handleActionDown(x, y, index);
+        if (joy1._button() != buttonState[0]) {
+          tank1.toggleFire(joy1._button());
+        }
+
         joy2.handleActionDown(x, y, index);
+        if (joy2._button() != buttonState[1]) {
+          tank2.toggleFire(joy2._button());
+        }
         break;
       case MotionEvent.ACTION_POINTER_DOWN:
         // Subsequent touch events
         joy1.handleActionDown(x, y, index);
+        if (joy1._button() != buttonState[0]) {
+          tank1.toggleFire(joy1._button());
+        }
+
         joy2.handleActionDown(x, y, index);
+        if (joy2._button() != buttonState[1]) {
+          tank2.toggleFire(joy2._button());
+        }
         break;
       case MotionEvent.ACTION_MOVE:
         // User swipes
@@ -257,15 +299,29 @@ public class GameView extends SurfaceView implements Runnable {
         // Every touch release except the last one
         joy1.handleActionUp(x, y, index);
         tank1.setAcc(joy1._xAcc(), joy1._yAcc());
+        if (joy1._button() != buttonState[0]) {
+          tank1.toggleFire(joy1._button());
+        }
+
         joy2.handleActionUp(x, y, index);
         tank2.setAcc(joy1._xAcc(), joy1._yAcc());
+        if (joy2._button() != buttonState[1]) {
+          tank2.toggleFire(joy2._button());
+        }
         break;
       case MotionEvent.ACTION_UP:
         // Last touch to be released
         joy1.handleActionUp(x, y, index);
         tank1.setAcc(joy1._xAcc(), joy1._yAcc());
+        if (joy1._button() != buttonState[0]) {
+          tank1.toggleFire(joy1._button());
+        }
+
         joy2.handleActionUp(x, y, index);
         tank2.setAcc(joy1._xAcc(), joy1._yAcc());
+        if (joy2._button() != buttonState[1]) {
+          tank2.toggleFire(joy2._button());
+        }
         break;
     }
     return true;
@@ -286,5 +342,71 @@ public class GameView extends SurfaceView implements Runnable {
     isRunning = true;
     gameThread = new Thread(this);
     gameThread.start();
+  }
+
+  static int getCollision(float x, float y, float xVel, float yVel, int size, int[] level) {
+    // First check edges of the space
+    int collision = 0;
+    if (x < 0 || x > TILE_SIZE * 25) {
+      collision++;
+    }
+    if (y < 0 || y > TILE_SIZE * 20) {
+      collision += 2;
+    }
+
+    boolean towardX, towardY;
+    int left, right, top, bottom;
+    float closestX, closestY, distX, distY;
+    // Check all tiles in the level
+    int tileCount = level.length;
+    for (int i = 0; i < tileCount; i++) {
+      // We break the loop if we've definitely found a corner collision
+      if (collision == 3) {
+        break;
+      }
+      // Only check wall tiles
+      if (level[i] == 3 || level[i] == 4) {
+        left = TILE_SIZE * (i % 25);
+        right = left + TILE_SIZE;
+        top = TILE_SIZE * ((int) i / 25);
+        bottom = top + TILE_SIZE;
+
+        closestX = clamp(x, left, right);
+        closestY = clamp(y, top, bottom);
+
+        distX = x - closestX;
+        distY = y - closestY;
+
+        if ((distX * distX) + (distY * distY) < size * size) {
+          if (Math.abs(distX) > Math.abs(distY)) {
+            // Determine whether player is moving towards the colliding tile
+            towardX = (xVel > 0 && right > x) || (xVel < 0 && left < x);
+            if (towardX && collision % 2 == 0) {
+              // We haven't found an x-axis collision yet
+              collision++;
+            }
+          }
+          else {
+            towardY = (yVel > 0 && bottom > y) || (yVel < 0 && top < y);
+            if (towardY && collision < 2) {
+              // We haven't found a y-axis collision yet
+              collision += 2;
+            }
+          }
+        }
+      }
+    }
+
+    return collision;
+  }
+
+  static int clamp(int value, int min, int max) {
+    return value < min ? min : (value > max ? max : value);
+  }
+  static float clamp(float value, int min, int max) {
+    return value < min ? min : (value > max ? max : value);
+  }
+  static float clamp(float value, float min, float max) {
+    return value < min ? min : (value > max ? max : value);
   }
 }
