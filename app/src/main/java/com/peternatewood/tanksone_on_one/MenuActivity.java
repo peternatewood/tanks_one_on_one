074b7727a8ba345c3f27daefff17e0bfab0eb65d
Peter Wood <pwood@sankyinc.com>
2018-03-30 11:55:26 -0400

    Change from MenuActivity to GameActivity if both players are ready

--- app/src/main/java/com/peternatewood/tanksone_on_one/MenuActivity.java
+++ app/src/main/java/com/peternatewood/tanksone_on_one/MenuActivity.java
@@ -4,12 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CompoundButton;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
 public class MenuActivity extends AppCompatActivity {
+  private boolean[] playersReady = { false, false };
   private String[] controls = { "joy", "joy" };
 
   @Override
@@ -33,7 +35,21 @@ public class MenuActivity extends AppCompatActivity {
     }
   }
 
-  public void startBattle(View v) {
+  public void togglePlayerReady(View v) {
+    CompoundButton button = (CompoundButton) v;
+    if (button != null) {
+      int playerNum = Integer.parseInt(v.getTag().toString());
+      if (playerNum == 0 || playerNum == 1) {
+        playersReady[playerNum] = button.isChecked();
+
+        if (playersReady[0] && playersReady[1]) {
+          startBattle(v);
+        }
+      }
+    }
+  }
+
+  private void startBattle(View v) {
     Intent intent = new Intent(this, GameActivity.class);
     intent.putExtra("controls0", controls[0]);
     intent.putExtra("controls1", controls[1]);
